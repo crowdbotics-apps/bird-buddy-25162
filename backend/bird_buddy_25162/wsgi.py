4ad6d4b7d8a027c2d5708baec6256e8dbb96bff7
Crowdbotics <team@crowdbotics.com>
2021-03-21 14:02:42 +0000

    Update models

--- backend/bird_buddy_25162/wsgi.py
+++ backend/bird_buddy_25162/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bird_buddy_25162.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bird_buddy_25162.settings")
 
 application = get_wsgi_application()
